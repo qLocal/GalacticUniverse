05546f2332a31ce415a51774a76c683c09de748f
qLocal <69441630+qLocal@users.noreply.github.com>
2020-08-15 01:31:02 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,7 @@
-function printYes()
-  print('Yes')
-end
- 
+game.Players.PlayerAdded:Connect(function(player)
+    player.Chatted:Connect(function(msg)
+        if msg = 'checkRepo' then
+          print('repo has successfully been required')
+          end
+        end)
+    end)
